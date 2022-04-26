eb19f4a42f8cf7e0abca8c9994c03d9131ace0e8
Aleksas Pielikis <ant.kampo@gmail.com>
2022-04-26 22:33:56 +0300

    Squash two libs into one (#5)

---
+++ simplerpc/src/main/java/io/github/aleksas/simplerpc/Return.java
@@ -0,0 +1,35 @@
+package io.github.aleksas.simplerpc;
+
+/**
+ * Method return class.
+ */
+public class Return {
+    public String doc;
+    public Object fmt;
+    public String tyme_name = "";
+
+    @Override  
+    public boolean equals(Object obj)   
+    {  
+        if (obj == null)   
+            return false;  
+        if (obj == this)  
+            return true;  
+        if (obj instanceof Return) {
+            var ret = (Return) obj;
+            return (doc == ret.doc || doc.equals(ret.doc)) &&
+                (fmt == ret.fmt || fmt.equals(ret.fmt)) &&
+                (tyme_name == ret.tyme_name || tyme_name.equals(ret.tyme_name));
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = (this.doc != null ? this.doc.hashCode() : 0);
+        hash = 100 * hash + (this.fmt != null ? this.fmt.hashCode() : 0);
+        hash = 100 * hash + (this.tyme_name != null ? this.tyme_name.hashCode() : 0);
+        return hash;
+    }
+}
---
+++ simplerpc/src/test/java/io/github/aleksas/simplerpc/SerialTest.java
@@ -0,0 +1,20 @@
+package io.github.aleksas.simplerpc;
+
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+
+import io.github.aleksas.simplerpc.Interface;
+import io.github.aleksas.simplerpc.serial.Serial;
+
+@DisabledIfEnvironmentVariable(named="CI", matches="true")
+public class SerialTest extends DeviceTest<Interface> {
+    @Override
+    protected Interface createInstance() {
+        try {
+            var transport = new Serial(Config.DEVICES.get("serial"), true, 9600);
+            return new Interface(transport, 0, false, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }    
+}
