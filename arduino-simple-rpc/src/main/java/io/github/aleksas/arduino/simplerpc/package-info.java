d18f62b79716715dda3e11f35cc6d10b14a751ab
Aleksas Pielikis <ant.kampo@gmail.com>
2022-05-14 21:24:15 +0300

    Add package description. Version bump.

---
+++ arduino-simple-rpc/src/main/java/io/github/aleksas/arduino/simplerpc/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This library provides a simple way to interface to Arduino functions exported with the simpleRPC protocol.
+ * The exported method definitions are communicated to the host,
+ * which is then able to generate an API interface using this library.
+ * 
+ * <h3>Features:</h3>
+ * <ul>
+ * <li>User friendly API library.</li>
+ * <li>UCommand line interface (CLI) for method discovery and testing.</li>
+ * <li>UFunction and parameter names are defined on the Arduino.</li>
+ * <li>UAPI documentation is defined on the Arduino.</li>
+ * <li>USupport for disconnecting and reconnecting.</li>
+ * <li>USupport for serial and ethernet devices.</li>
+ * </ul>
+ */
+package io.github.aleksas.arduino.simplerpc;
